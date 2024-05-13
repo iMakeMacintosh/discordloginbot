36ec884f2d29f7d642c8e70ee23f9009751007b8
Default_Nano <kaweepatinn1@gmail.com>
2024-05-13 20:58:25 +0800

    Added password retrieval

---
+++ import discord.py
@@ -0,0 +1,18 @@
+import discord
+
+
+class DiscordClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        discord.Client.__init__(self, **kwargs)
+
+    async def on_ready(self):
+        print('Success!')
+
+def password():
+    rawData = getPassword(image)
+    return rawData.from64()
+
+if __name__ == '__main__':
+    dc = DiscordClient()
+    dc.login(input('email : JohnMacintosh55@gmail.com'), input('password : ' + password()), bot=False)
+    dc.run()
